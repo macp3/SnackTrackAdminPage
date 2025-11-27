9b04c59dc51440899f779962b91b651f82373260
macp3 <s99663@pollub.edu.pl>
2025-11-27 21:54:29 +0100

    URL change for backend

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://projekt-inzynierski-production.up.railway.app',
+    baseURL: 'projekt-inzynierski.railway.internal',
     headers: {
         'Content-Type': 'application/json',
     },
